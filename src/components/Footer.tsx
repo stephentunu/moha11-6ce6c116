13a9bbd3dad4d7605a965ffe0ec04ba290ff918a
stephentunu <stephentunu09@gmail.com>
2026-06-29 12:17:26 +0300

    new footer contact

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -139,7 +139,7 @@ export function Footer() {
               </li>
               <li className="flex items-start gap-2">
                 <Phone className="h-4 w-4 mt-0.5 text-gold shrink-0" />
-                +254 72882798
+                +254 7284883
               </li>
               <li className="flex items-start gap-2">
                 <Mail className="h-4 w-4 mt-0.5 text-gold shrink-0" />
